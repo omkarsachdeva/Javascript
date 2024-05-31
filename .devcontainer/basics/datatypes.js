69d41148a2a92700182d26990a67e5372ff8595e
omkarsachdeva <125148365+omkarsachdeva@users.noreply.github.com>
2024-05-31 09:07:18 +0000

    variables/datatypes

---
+++ .devcontainer/basics/datatypes.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log(3+3)
+console.log("omkar")
+let name="omkar"
+let age =20
+let isLoggedIn=false
+let state;
+console.log(typeof isLoggedIn)
---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,11 @@
+const accountId=1234
+let accountEmail="omkar@gmail.com"
+var accountPass="12345"
+accountCity="jaipur"
+//accountId=2
+console.log(accountId);
+accountEmail="omkar123@gmail.com"
+ accountPass="123456"
+accountCity="jbangalore"
+let accountState
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
