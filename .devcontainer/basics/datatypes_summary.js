66b700d8d68904f12ef5498035270986256c7707
omkarsachdeva <125148365+omkarsachdeva@users.noreply.github.com>
2024-06-02 08:16:35 +0000

    updates

--- .devcontainer/basics/datatypes_summary.js
+++ .devcontainer/basics/datatypes_summary.js
@@ -1,8 +1,21 @@
-const id=Symbol('123')
-const anotherid=Symbol('123')
-console.log(id==anotherid);
-const outtemp=null
-const myFunction=function(){
-    console.log("hello world");
-}
-console.log(typeof myFunction);
+// const id=Symbol('123')
+// const anotherid=Symbol('123')
+// console.log(id==anotherid);
+// const outtemp=null
+// const myFunction=function(){
+//     console.log("hello world");
+// }
+// console.log(typeof myFunction);
+
+// stack(primitive)
+
+
+            // let yt="omkar"
+            // let anotheryt=yt
+            // anotheryt="sachdeva"
+            // console.log(yt);
+            // console.log(anotheryt);
+
+// heap(non primitive)
+
+//  
