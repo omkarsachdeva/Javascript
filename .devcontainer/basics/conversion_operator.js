5abbba7be9176cb6d0e79a8d16266adae9364c40
omkarsachdeva <125148365+omkarsachdeva@users.noreply.github.com>
2024-06-02 05:06:35 +0000

    conversion_operator

---
+++ .devcontainer/basics/conversion_operator.js
@@ -0,0 +1,88 @@
+//let score="om"
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+
+//let value=Number(score)
+//console.log(typeof value);
+//console.log(value);
+
+
+
+// let isLoggedIn=undefined
+// let bool=Boolean(isLoggedIn)
+// console.log(bool)
+
+
+
+
+// let stringNumber=33
+// let string=String(stringNumber)
+// console.log(typeof string)
+
+
+
+
+
+// let val=25
+// let negVal=-val
+// console.log(negVal);
+
+
+
+
+
+// let st1="hello"
+// let st2=" omkar"
+// let st3=st1+st2
+// console.log(st3);
+
+
+
+
+// console.log("1"+2)
+// console.log("2"+1)
+// console.log("1"+2+2)
+// console.log(2+"2"+"1")
+// console.log(1+2+"3")
+
+
+
+// console.log(+true);
+// console.log(+"");
+
+
+
+
+
+// let n1,n2,n3
+// n1=n2=n3=2+2
+// console.log(n1);
+// console.log(n2);
+// console.log(n3);
+
+
+
+
+// let x = 3;
+// let y = x++;
+// console.log(`x:${x}, y:${y}`);
+// let a = 3;
+// let b = ++a;
+
+
+
+
+// console.log(`a:${a}, b:${b}`);
+// console.log("2">1);
+// console.log("02">1);
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+
+
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
