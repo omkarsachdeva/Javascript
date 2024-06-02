80d3b19e0804d54e7c0b176b9c9bef4dbe2fe6ca
omkarsachdeva <125148365+omkarsachdeva@users.noreply.github.com>
2024-06-02 08:00:28 +0000

    datatype_summary

---
+++ .devcontainer/basics/datatypes_summary.js
@@ -0,0 +1,8 @@
+const id=Symbol('123')
+const anotherid=Symbol('123')
+console.log(id==anotherid);
+const outtemp=null
+const myFunction=function(){
+    console.log("hello world");
+}
+console.log(typeof myFunction);
