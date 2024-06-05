f6226f16883a5ad99592cdfc337392f33689f378
omkarsachdeva <125148365+omkarsachdeva@users.noreply.github.com>
2024-06-05 08:31:22 +0000

    math and num

---
+++ .devcontainer/basics/nums_and_math.js
@@ -0,0 +1,29 @@
+const score=300
+//console.log(score);
+const bal=new Number(100)
+// console.log(bal);
+// console.log(bal.toString());
+// console.log(bal.toString().length);
+// console.log(bal.toFixed(2));
+const b=124.434
+//console.log(b.toPrecision(4));
+const hundreds=1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//////////////////////////////////
+
+
+// console.log(Math);
+// console.log(Math.abs(4));
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(4,3,5,6));
+// console.log(Math.max(4,78,9,6));
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10)+1);
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
