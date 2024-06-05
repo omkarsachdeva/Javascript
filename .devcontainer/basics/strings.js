9f571b594254a7e290e2ab4fa39f4d784ca0d7d8
omkarsachdeva <125148365+omkarsachdeva@users.noreply.github.com>
2024-06-05 05:37:33 +0000

    strings

---
+++ .devcontainer/basics/strings.js
@@ -0,0 +1,25 @@
+const name="omkar"
+const rep=50
+//console.log(name+rep+"value")
+
+console.log(`Hello my name is ${name} and my repo count is ${rep}`);
+const game=new String("omkar_os_com")
+    console.log(game[2]);
+    console.log(game.__proto__);
+    console.log(game.length);
+    console.log(game.toUpperCase());
+    console.log(game.charAt(2));
+    console.log(game.indexOf('m'));
+   const newstring=game.substring(0,4) 
+   console.log(newstring);
+   const astring=game.slice(-7,4)
+   console.log(astring);
+   const abs="  omkar  "
+   console.log(abs);
+   console.log(abs.trim());
+   const url="https://omkaar.com/omkar%30sachdeva"
+   console.log(url.replace('%30','_'));
+   console.log(url.includes('sunder'));
+   console.log(url.includes('omkar'));
+   console.log(game.split('_'));
+   
