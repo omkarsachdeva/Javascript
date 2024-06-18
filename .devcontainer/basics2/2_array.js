5933fac3f9cc1e23e1aa43fce4260906799aee02
omkarsachdeva <125148365+omkarsachdeva@users.noreply.github.com>
2024-06-18 12:57:35 +0000

    array

---
+++ .devcontainer/basics2/2_array.js
@@ -0,0 +1,19 @@
+// const marvel_heros=["thor","ironman","spiderman"]
+// const dc_heros=["superman","batman","flash"]
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][2]);
+// const allheros=marvel_heros.concat(dc_heros)
+// console.log(allheros);
+// const all_new_heros=[...marvel_heros,...dc_heros]
+// console.log(all_new_heros);
+// const another_array=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real_another_array=another_array.flat(Infinity)
+// console.log(real_another_array);
+// console.log(Array.isArray("omkar"));
+// console.log(Array.from("omkar"));
+// console.log(Array.from({name:"omkar"}));// special case will always give empty array
+let score1=100
+let score2=300
+let score3=400
+console.log(Array.of(score1,score2,score3));
---
+++ .devcontainer/basics2/array.js
@@ -0,0 +1,32 @@
+const myArr=[2,3,4,4,6,5,7]
+// const myHeroes=["shaktiman","naagraj"]
+// console.log(myArr[2]);
+// console.log(myHeroes[1]);
+// const myArr2=new Array(1,2,3,4)
+// console.log(myArr2[1]);
+// myArr.push(8)
+// myArr.push(7)
+// myArr.pop()
+// myArr.unshift(0)
+// myArr.shift()
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+// console.log(myArr.indexOf(5));
+//  const newArr=myArr.join()
+// const newArr=[1,5,8,9,4]
+
+// console.log( typeof newArr);
+
+// console.log(myArr);
+// console.log(newArr.concat(myArr));
+console.log("A",myArr);
+
+
+const myn=myArr.slice(1,3)
+console.log(myn);
+console.log("B",myArr);
+
+
+const myn2=myArr.splice(1,3)
+console.log("C",myArr);
+console.log(myn2);
